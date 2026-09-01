ae7e133308e1ea9849280aa7b43c2e8266900c8e
yfree1232-afk <yfree1232@gmail.com>
2026-09-01 18:58:26 +0530

    Update config.py for Save-Restricted-Content-Bot

--- config.py
+++ config.py
@@ -45,8 +45,8 @@ FREEMIUM_LIMIT = int(os.getenv("FREEMIUM_LIMIT", "0"))
 PREMIUM_LIMIT  = int(os.getenv("PREMIUM_LIMIT", "500"))
 
 # ─── UI / LINKS ─────────────────────────────────────────────────────────────────
-JOIN_LINK     = os.getenv("JOIN_LINK", "https://t.me/team_spy_pro")
-ADMIN_CONTACT = os.getenv("ADMIN_CONTACT", "https://t.me/username_of_admin")
+JOIN_LINK     = os.getenv("JOIN_LINK", "https://t.me/voltexbots")
+ADMIN_CONTACT = os.getenv("ADMIN_CONTACT", "https://t.me/voltexbots")
 
 # ════════════════════════════════════════════════════════════════════════════════
 # ░ PREMIUM PLANS CONFIGURATION
