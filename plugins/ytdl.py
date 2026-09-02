7af9d4ee797201464fa2fc1a442c2ef097fbb9f9
yfree1232-afk <yfree1232@gmail.com>
2026-09-02 21:34:44 +0530

    Update plugins/ytdl.py for Save-Restricted-Content-Bot

--- plugins/ytdl.py
+++ plugins/ytdl.py
@@ -89,7 +89,7 @@ async def process_audio(client, event, url, cookies_env_var=None):
             temp_cookie_path = temp_cookie_file.name
  
     start_time = time.time()
-    random_filename = f"@team_spy_pro_{event.sender_id}"
+    random_filename = f"@voltexbots_{event.sender_id}"
     download_path = f"{random_filename}.mp3"
  
     ydl_opts = {
